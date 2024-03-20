93d4ff6b78eb13767b361f395f6e90cf2544d639
sociallyencrypted <mehularora810@gmail.com>
2024-03-20 15:03:33 +0530

    updated client code

--- client/client.py
+++ client/client.py
@@ -1,18 +1,34 @@
 import grpc
 from protos import raft_pb2, raft_pb2_grpc
+import time
 
 class RaftClient:
     def __init__(self, node_addresses):
         self.node_addresses = node_addresses
         self.leader_id = None
-        # Establish gRPC channel with one of the nodes
+        
 
     def get(self, key):
-        # Send GET request to leader
-        # Handle leader redirection and retry
-        pass
+        leader_ip = self.node_addresses[self.leader_id]
+        with grpc.insecure_channel(leader_ip) as channel:
+            stub = raft_pb2_grpc.RaftNodeStub(channel)
+            response = stub.ServeClient(raft_pb2.ClientRequest(action=raft_pb2.GET, key=key))
+            if response.leader_id: # This means the node to which the request was sent is not a leader. It hence sends a guess of who the leader is.
+                if response.leader_id == "NONE": # This means there is no leader. The client should try again later.
+                    time.sleep(0.1)
+                    return self.get(key)
+                self.leader_id = response.leader_id # Update the leader_id to the one guessed by the node,
+                return self.get(key) # and try again.
+            else:
+                if response.value:
+                    return response.value
+                else:
+                    return "Key not found"
 
     def set(self, key, value):
-        # Send SET request to leader
-        # Handle leader redirection and retry
-        pass
+        leader_ip = self.node_addresses[self.leader_id]
+        with grpc.insecure_channel(leader_ip) as channel:
+            stub = raft_pb2_grpc.RaftNodeStub(channel)
+            entry = f'{key}:{value}'
+            response = stub.ServeClient(raft_pb2.ClientRequest(action=raft_pb2.SET, entry=entry))
+            return response.value
